4bf6c944b295108275a4cffdfa857836787fac75
kecheon <kecheon@gmail.com>
2020-10-27 05:50:55 +0900

    change style to argo-ui style

--- ui/src/app/devstack/register/components/register.tsx
+++ ui/src/app/devstack/register/components/register.tsx
@@ -1,6 +1,7 @@
 import {Page} from 'argo-ui';
 import * as React from 'react';
-import {Button, Form} from 'react-bootstrap';
+import { Form, Text } from 'react-form';
+import { FormField, FormSelect } from 'argo-ui';
 
 export class Register extends React.Component {
     public handleChange = (event: any) => {};
@@ -9,23 +10,23 @@ export class Register extends React.Component {
         return (
             <Page title='Register' toolbar={{breadcrumbs: [{title: 'Register'}]}}>
                 <div className='argo-container'>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Group controlId='formBasicUsername'>
-                            <Form.Label>User name</Form.Label>
-                            <Form.Control type='text' placeholder='Enter username' />
-                        </Form.Group>
-                        <Form.Group controlId='formBasicEmail'>
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type='email' placeholder='Enter email' />
-                            <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text>
-                        </Form.Group>
-                        <Form.Group controlId='formBasicPassword'>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type='password' placeholder='Password' />
-                        </Form.Group>
-                        <Button variant='primary' type='submit'>
-                            Submit
-                        </Button>
+                    <Form>
+                        {(api) => (
+                            <form style={{padding: '1em'}}>
+                                <div className='argo-form-row'>
+                                    <FormField label='Username' formApi={api} field='textField' component={Text} />
+                                </div>
+                                <div className='argo-form-row'>
+                                    <FormField label='Email' formApi={api} field='textField' component={Text} />
+                                </div>
+                                <div className='argo-form-row'>
+                                    <FormField label='Password' formApi={api} field='passwordField' component={Text} componentProps={{type: 'password'}} />
+                                </div>
+                                <div className='argo-form-row'>
+                                    <FormField label='Select' formApi={api}  field='selectField' component={FormSelect} componentProps={{options: ['option1', 'option2']}} />
+                                </div>
+                            </form>
+                        )}
                     </Form>
                 </div>
             </Page>
