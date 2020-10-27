8e4b7e4b7b60ef831401014d2114425b11896a15
kecheon <kecheon@gmail.com>
2020-10-27 21:50:01 +0900

    refactor register component

--- ui/src/app/devstack/register/components/register.tsx
+++ ui/src/app/devstack/register/components/register.tsx
@@ -1,47 +1,77 @@
-import {FormField, FormSelect, Page} from 'argo-ui';
+import {Page} from 'argo-ui';
 import * as React from 'react';
-import {Form, Text} from 'react-form';
+import {Button, Col, Container, Form, FormControl, InputGroup, Row} from 'react-bootstrap';
 
-interface State {
-    signUpProgress: boolean;
-}
-
-export class Register extends React.Component<State> {
-    // constructor(props: {}) {
-    //     super(props);
-    //     this.state = {signUpProgress: false};
-    // };
-    // public state: State = {
-    //     signUpProgress: false
-    // };
-    // public handleChange = (event: any) => {};
-    // public handleSubmit = (event: any) => {};
+export class Register extends React.Component {
+    public handleChange = (event: any) => {};
+    public handleSubmit = (event: any) => {};
     public render() {
         return (
             <Page title='Register' toolbar={{breadcrumbs: [{title: 'Register'}]}}>
                 <div className='argo-container'>
-                    <Form>
-                        {(api) => (
-                            <form style={{padding: '1em'}}>
-                                <div className='argo-form-row'>
-                                    <FormField label='Username' formApi={api} field='textField' component={Text} />
-                                </div>
-                                <div className='argo-form-row'>
-                                    <FormField label='Email' formApi={api} field='textField' component={Text} />
-                                </div>
-                                <div className='argo-form-row'>
-                                    <FormField label='Password' formApi={api} field='passwordField' component={Text} componentProps={{type: 'password'}} />
-                                </div>
-                                <div className='argo-form-row'>
-                                    <FormField label='Select' formApi={api}  field='selectField' component={FormSelect} componentProps={{options: ['option1', 'option2']}} />
-                                </div>
-                                <div className='argo-form-row'>
-                                    <button className='argo-button argo-button--full-width argo-button--xlg' type='submit'>
-                                        Register me
-                                    </button>
-                                </div>
-                            </form>
-                        )}
+                    <div>
+                        <h3>New Tenant</h3>
+                        Create user(admin) and namespace for workflow
+                    </div>
+                    <Button variant='primary' type='submit'>
+                        Submit
+                    </Button>
+                    <Button variant='secondary' type='submit'>
+                        Cancel
+                    </Button>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Form.Group controlId='formBasicUsername'>
+                                        <Form.Label>User Name*</Form.Label>
+                                        <Form.Control type='text' placeholder='Enter username' />
+                                    </Form.Group>
+                                    <Form.Group controlId='formBasicDescription'>
+                                        <Form.Label>Description</Form.Label>
+                                        <Form.Control as='textarea' placeholder='Enter description' />
+                                    </Form.Group>
+                                    <Form.Group controlId='formBasicEmail'>
+                                        <Form.Label>Description</Form.Label>
+                                        <Form.Control type='email' placeholder='Enter email' />
+                                    </Form.Group>
+                                    <Form.Group controlId='formBasicPassword1'>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type='password' placeholder='Password' />
+                                    </Form.Group>
+                                    <Form.Group controlId='formBasicPassword2'>
+                                        <Form.Label>Confirm Password</Form.Label>
+                                        <Form.Control type='password' placeholder='Password again' />
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group controlId='formBasicEmail'>
+                                        <Form.Label>Namespace Name*</Form.Label>
+                                        <Form.Control type='string' placeholder='Enter namespace' />
+                                        <Form.Text className='text-muted'>We'll never share your email with anyone else.</Form.Text>
+                                    </Form.Group>
+                                    <Form.Group controlId='formBasicDescriptionNamespace'>
+                                        <Form.Label>Namespace Description</Form.Label>
+                                        <Form.Control as='textarea' placeholder='Enter namespace description' />
+                                    </Form.Group>
+                                    <Form.Label>Quota</Form.Label>
+                                    <InputGroup>
+                                        <FormControl placeholder='CPU' />
+                                        <InputGroup.Append>
+                                            <Button variant='outline-secondary'>+</Button>
+                                            <Button variant='outline-secondary'>-</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                    <InputGroup>
+                                        <FormControl placeholder='Memory' />
+                                        <InputGroup.Append>
+                                            <Button variant='outline-secondary'>+</Button>
+                                            <Button variant='outline-secondary'>-</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Form>
                 </div>
             </Page>
