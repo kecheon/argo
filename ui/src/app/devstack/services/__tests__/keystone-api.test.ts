5bbe8c8140783249246fbd8484e637fefa07f64a
kecheon <kecheon@gmail.com>
2020-10-31 12:00:20 +0900

    refactor tests

--- ui/src/app/devstack/services/__tests__/keystone-api.test.ts
+++ ui/src/app/devstack/services/__tests__/keystone-api.test.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 const endpoint = 'http://183.111.177.141/identity/v3';
+<<<<<<< HEAD
 
 xdescribe('API access', () => {
     it('get unscoped token by username and password', async done => {
+=======
+describe('API access', () => {
+    it('get unscoped token by username and password', async () => {
+>>>>>>> 122b46ff... keystone auth test with username, password ok; CORS error was from node
         const data = {
             auth: {
                 identity: {
@@ -17,5 +22,6 @@ xdescribe('API access', () => {
 
         const response = await axios.post(url, data);
         expect(response.status).toEqual(201);
+        expect(response.headers['x-subject-token'].length > 0).toBeTruthy();
     });
 });
