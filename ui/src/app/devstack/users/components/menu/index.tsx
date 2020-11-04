1e7d2e2b3d2941ad465d105eedd3d0f9185edf36
kecheon <kecheon@gmail.com>
2020-11-04 09:52:33 +0900

    User management main page setup ok

--- ui/src/app/devstack/users/components/menu/index.tsx
+++ ui/src/app/devstack/users/components/menu/index.tsx
@@ -1,23 +1,42 @@
 import * as React from 'react';
 import {Page} from 'argo-ui';
+require('./menu.scss');
+import {uiUrl} from '../../../../shared/base';
 
-export default () => {
-  return (
-    <Page title='User management' toolbar={{breadcrumbs: [{title: 'User management'}]}}>
+const menu = [
+  {
+      title: 'Manage Users',
+      description: 'Create/update/delete User',
+      path: 'userinfo'
+  },
+  {
+      title: 'Manage Namespaces',
+      description: 'Create/update/delete Namespaces',
+      path: 'userinfo'
+  },
+  {
+      title: 'Manage Roles',
+      description: 'View Roles',
+      path: 'userinfo'
+  }
+]
+
+export const UsersOverview = () => (
+  <Page title='User management' toolbar={{breadcrumbs: [{title: 'User management'}]}}>
+    <div className='settings-overview'>
       <div className='argo-container'>
-        <div className='row argo-table-list__row'>
-            <h3>Manage Users</h3>
-            <div>Create/update/delete User</div>
-        </div>
-        <div className='row argo-table-list__row'>
-            <h3>Manage Namespaces</h3>
-            Create/update/delete Namespaces
-        </div>
-        <div className='row argo-table-list__row'>
-            <h3>Manage Roles</h3>
-            View Role
-        </div>
+        {menu.map(item => (
+              <div key={item.path} className='settings-overview__redirect-panel' onClick={() => document.location.href=uiUrl(item.path)}>
+                  <div className='settings-overview__redirect-panel__content'>
+                      <div className='settings-overview__redirect-panel__title'>{item.title}</div>
+                      <div className='settings-overview__redirect-panel__description'>{item.description}</div>
+                  </div>
+                  <div className='settings-overview__redirect-panel__arrow'>
+                      <i className='fa fa-angle-right' />
+                  </div>
+              </div>
+          ))}
       </div>
-    </Page>
-  );
-}
+    </div>
+  </Page>
+);
--- ui/src/app/devstack/users/components/users-container.tsx
+++ ui/src/app/devstack/users/components/users-container.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import {Redirect, Route, RouteComponentProps, Switch} from 'react-router';
-import menu from './menu';
+import {UsersOverview} from './menu';
 
 export const UsersContainer = (props: RouteComponentProps<any>) => (
     <div className='argo-container'>
         <Switch>
-            <Route exact={true} path={`${props.match.path}`} component={menu} />
+            <Route exact={true} path={`${props.match.path}`} component={UsersOverview} />
             <Redirect path='*' to={`${props.match.path}`} />
         </Switch>
     </div>
