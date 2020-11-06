91ffcf365e034c519ea3ca54a82a39443aba4335
kecheon <kecheon@gmail.com>
2020-11-06 21:25:42 +0900

    activate cancel button in create namespace component via context consume

--- ui/src/app/devstack/users/components/namespaces/components/create-namespace/create-namespace.tsx
+++ ui/src/app/devstack/users/components/namespaces/components/create-namespace/create-namespace.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Form} from 'react-bootstrap';
 import { useState } from 'react';
 import { NamespaceService } from '../../../../../services/namespace-service';
+import {Consumer} from '../../../../../../shared/context';
 
 export interface NamespaceForm {
   name: string;
@@ -43,71 +44,73 @@ export default () => {
       }
   }
   return (
-    <div className=''>
-      <form onSubmit={ handleSubmit }>
-          <div className='login__form-row'>
-              <button className='argo-button argo-button--base' type='submit'>
-                  <i className='fa fa-plus-circle' /> Submit
-              </button>
-              <button className='argo-button argo-button--base' type='button'>
-                <i className='fa fa-times-circle' /> Cancel
-              </button>
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicName'>
-                  <Form.Label>Name*</Form.Label>
-                  <Form.Control type='text' placeholder='Enter namespace name'
-                  value={ name }
-                  onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setName(e.target.value)}/>
-              </Form.Group>{' '}
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicUserId'>
-                  <Form.Label>Member</Form.Label>
-                  <Form.Control type='text' placeholder='User id'
-                  value={ userId }
-                  onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setUserId(e.target.value)}/>
-              </Form.Group>
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicRole'>
-                  <Form.Label>Role</Form.Label>
-                  <Form.Control type='text' placeholder='Role'
-                  value={ role }
-                  onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setRole(e.target.value)}/>
-              </Form.Group>
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicCpu'>
-                  <Form.Label>CPU</Form.Label>
-                  <Form.Control type='number' placeholder='CPU'
-                  value={ cpu }
-                  onChange={(e: { target: { value: React.SetStateAction<number>; }; }) => setCpu(e.target.value)}/>
-              </Form.Group>
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicMemory'>
-                  <Form.Label>Memory</Form.Label>
-                  <Form.Control type='number' placeholder='memory size'
-                  value={ memory }
-                  onChange={(e: { target: { value: React.SetStateAction<number>; }; }) => setMemory(e.target.value)}/>
-              </Form.Group>
-          </div>
-          <div className='argo-form-row'>
-              <Form.Group controlId='formBasicEnabled'>
-                  <Form.Label>Enabled</Form.Label>
-                  <Form.Control type='boolean' placeholder='Enabled'
-                  value={ enabled }
-                  onChange={(e: { target: { value: React.SetStateAction<boolean>; }; }) => setEnabled(e.target.value)}/>
-              </Form.Group>
-          </div>
-          
-          <div className='login__footer'>
-              <a href='https://argoproj.io' target='_blank'>
-                  <img className='logo-image' src='assets/images/argologo.svg' alt='argo' />
-              </a>
-          </div>
-      </form>
-    </div>  );
+    <Consumer>
+        {ctx => (
+            <form onSubmit={ handleSubmit }>
+                <div className='login__form-row'>
+                    <button className='argo-button argo-button--base' type='submit'>
+                        <i className='fa fa-plus-circle' /> Submit
+                    </button>
+                    <button className='argo-button argo-button--base' type='button' onClick={() => ctx.navigation.goto('.', {new: null})}>
+                        <i className='fa fa-times-circle' /> Cancel
+                    </button>
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicName'>
+                        <Form.Label>Name*</Form.Label>
+                        <Form.Control type='text' placeholder='Enter namespace name'
+                        value={ name }
+                        onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setName(e.target.value)}/>
+                    </Form.Group>{' '}
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicUserId'>
+                        <Form.Label>Member</Form.Label>
+                        <Form.Control type='text' placeholder='User id'
+                        value={ userId }
+                        onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setUserId(e.target.value)}/>
+                    </Form.Group>
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicRole'>
+                        <Form.Label>Role</Form.Label>
+                        <Form.Control type='text' placeholder='Role'
+                        value={ role }
+                        onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setRole(e.target.value)}/>
+                    </Form.Group>
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicCpu'>
+                        <Form.Label>CPU</Form.Label>
+                        <Form.Control type='number' placeholder='CPU'
+                        value={ cpu }
+                        onChange={(e: { target: { value: React.SetStateAction<number>; }; }) => setCpu(e.target.value)}/>
+                    </Form.Group>
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicMemory'>
+                        <Form.Label>Memory</Form.Label>
+                        <Form.Control type='number' placeholder='memory size'
+                        value={ memory }
+                        onChange={(e: { target: { value: React.SetStateAction<number>; }; }) => setMemory(e.target.value)}/>
+                    </Form.Group>
+                </div>
+                <div className='argo-form-row'>
+                    <Form.Group controlId='formBasicEnabled'>
+                        <Form.Label>Enabled</Form.Label>
+                        <Form.Control type='boolean' placeholder='Enabled'
+                        value={ enabled }
+                        onChange={(e: { target: { value: React.SetStateAction<boolean>; }; }) => setEnabled(e.target.value)}/>
+                    </Form.Group>
+                </div>
+                
+                <div className='login__footer'>
+                    <a href='https://argoproj.io' target='_blank'>
+                        <img className='logo-image' src='assets/images/argologo.svg' alt='argo' />
+                    </a>
+                </div>
+            </form>
+        )}
+    </Consumer>
+  );
 }
-
