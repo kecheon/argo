39092262a62f59351963fd12216c46117c7eff77
kecheon <kecheon@gmail.com>
2020-11-12 15:41:17 +0900

    refactor users-overview

--- ui/src/app/devstack/users/components/users-overview/index.tsx
+++ ui/src/app/devstack/users/components/users-overview/index.tsx
@@ -21,22 +21,27 @@ const menu = [
   }
 ]
 
-export const UsersOverview = () => (
-  <Page title='User management' toolbar={{breadcrumbs: [{title: 'User management'}]}}>
-    <div className='settings-overview'>
-      <div className='argo-container'>
-        {menu.map(item => (
-              <div key={item.path} className='settings-overview__redirect-panel' onClick={() => document.location.href=uiUrl(`users/${item.path}`)}>
-                  <div className='settings-overview__redirect-panel__content'>
-                      <div className='settings-overview__redirect-panel__title'>{item.title}</div>
-                      <div className='settings-overview__redirect-panel__description'>{item.description}</div>
-                  </div>
-                  <div className='settings-overview__redirect-panel__arrow'>
-                      <i className='fa fa-angle-right' />
-                  </div>
-              </div>
-          ))}
+export const UsersOverview = () => {
+  const clickHandler = (path: string) => {
+    document.location.href = uiUrl(`users/${path}`)
+  }
+  return (
+    <Page title='User management' toolbar={{breadcrumbs: [{title: 'User management'}]}}>
+      <div className='settings-overview'>
+        <div className='argo-container'>
+          {menu.map(item => (
+                <div key={item.path} className='settings-overview__redirect-panel' onClick={() => clickHandler(item.path)}>
+                    <div className='settings-overview__redirect-panel__content'>
+                        <div className='settings-overview__redirect-panel__title'>{item.title}</div>
+                        <div className='settings-overview__redirect-panel__description'>{item.description}</div>
+                    </div>
+                    <div className='settings-overview__redirect-panel__arrow'>
+                        <i className='fa fa-angle-right' />
+                    </div>
+                </div>
+            ))}
+        </div>
       </div>
-    </div>
-  </Page>
-);
+    </Page>
+  );
+}
