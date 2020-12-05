77df8f01d66e66d6fb5f15b727d90600fa6d0c06
kecheon <kecheon@gmail.com>
2020-12-05 20:51:07 +0900

    refactor edit user form

--- ui/src/app/devstack/users/components/users-list/components/workflow-drawer/workflow-drawer.tsx
+++ ui/src/app/devstack/users/components/users-list/components/workflow-drawer/workflow-drawer.tsx
@@ -10,7 +10,7 @@ import { User } from '../../../models';
 // import {WorkflowFrom} from '../workflow-from';
 // import {WorkflowLabels} from '../workflow-labels/workflow-labels';
 import {UserService} from '../../../../../services/user-service';
-import { Form } from 'react-bootstrap';
+import { Form, Col, Row } from 'react-bootstrap';
 import {Consumer} from '../../../../../../shared/context';
 
 require('./workflow-drawer.scss');
@@ -41,6 +41,9 @@ export class WorkflowDrawer extends React.Component<WorkflowDrawerProps, Workflo
             this.setState({userProfile});
         });
     }
+    public changeHandler = (e: any) => {
+        console.log(e);
+    }
 
     public render() {
         if (!this.state.userProfile) {
@@ -57,31 +60,44 @@ export class WorkflowDrawer extends React.Component<WorkflowDrawerProps, Workflo
                                 </button>
                             </div>
                             <div className='argo-form-row'>
-                                <Form.Group controlId='formBasicUsername'>
-                                    <Form.Label>User Name*</Form.Label>
-                                    <Form.Control type='text' placeholder='Enter username'
-                                    value={ this.state.userProfile.name } />
+                                <Form.Group as={Row} controlId='formBasicUsername'>
+                                    <Form.Label column={true} sm={2}>User Name*</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control type='text' placeholder='Enter username'
+                                            value={ this.state.userProfile.name } 
+                                            onChange={this.changeHandler}
+                                        />
+                                    </Col>
                                 </Form.Group>
                             </div>
                             <div className='argo-form-row'>
-                                <Form.Group controlId='formBasicDescription'>
-                                    <Form.Label>Description</Form.Label>
-                                    <Form.Control as='textarea' placeholder='Description'
-                                    value={ this.state.userProfile.description } />
+                                <Form.Group as={Row} controlId='formBasicDescription'>
+                                    <Form.Label column={true} sm={2}>Description</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control as='textarea' placeholder='Description'
+                                        value={ this.state.userProfile.description } 
+                                        onChange={this.changeHandler} />
+                                    </Col>
                                 </Form.Group>
                             </div>
                             <div className='argo-form-row'>
-                                <Form.Group controlId='formBasicEmail'>
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type='email' placeholder='Email'
-                                    value={ this.state.userProfile.email } />
+                                <Form.Group as={Row} controlId='formBasicEmail'>
+                                    <Form.Label column={true} sm={2}>Email</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control type='email' placeholder='Email'
+                                            onChange={this.changeHandler}
+                                            value={ this.state.userProfile.email } />
+                                    </Col>
                                 </Form.Group>
                             </div>
                             <div className='argo-form-row'>
-                                <Form.Group controlId='formBasicPassword1'>
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type='password' placeholder='password'
-                                    value={ this.state.userProfile.password } />
+                                <Form.Group as={Row} controlId='formBasicPassword1'>
+                                    <Form.Label column={true} sm={2}>Password</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Control type='password' placeholder='password'
+                                            onChange={this.changeHandler}
+                                            value={ this.state.userProfile.password } />
+                                    </Col>
                                 </Form.Group>
                             </div>
                         </form>
